9854baf85df6435a0a801c333675b5e361c3f206
ElFirdaous28 <elmokhtarifirdaous@gmqil.com>
2025-10-08 16:10:02 +0100

    update user EMA-18

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -2,10 +2,33 @@ import User from "../models/User.js";
 
 export const createUser = async (req, res) => {
   try {
-    const user = new User(req.body);
+    // if first user role = admin
+    const userCount = await User.countDocuments();
+    const role = userCount === 0 ? "admin" : req.body.role || "user";
+
+    const user = new User(req.body, role);
     await user.save();
 
     res.status(201).json({ message: "User created successfully", user });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: "Server error", error });
+  }
+};
+
+export const updateUser = async (req, res) => {
+  try {
+    const updatedUser = await User.findByIdAndUpdate(
+      req.params.id,
+      req.body,
+      { new: true, runValidators: true }
+    );
+
+    if (!updatedUser) {
+      return res.status(404).json({ error: "User not found" });
+    }
+
+    res.status(200).json({ message: "User updated", user: updatedUser });
   } catch (error) {
     console.error(error);
     res.status(500).json({ error: "Server error" });
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,10 +1,11 @@
 import express from "express";
 import * as userController from "../controllers/UserController.js";
 import validate from "../middlewares/validate.js";
-import { userSchema } from "../validations/userValidation.js";
+import { userSchema } from "../validations/userSchema.js";
 
 const router = express.Router();
 
 router.post("/", validate(userSchema), userController.createUser);
+router.patch("/:id", validate(userSchema), userController.updateUser);
 
 export default router;
