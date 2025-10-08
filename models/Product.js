600c6baaf67ee47685b6e906e6e27f0a42c76da2
ElFirdaous28 <elmokhtarifirdaous@gmqil.com>
2025-10-08 11:50:55 +0100

    models EMA-4 EMA-5 EMA-17

---
+++ models/Product.js
@@ -0,0 +1,38 @@
+import mongoose from "mongoose";
+
+const productSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: [true, "Product title is required"],
+    trim: true,
+  },
+  description: {
+    type: String,
+    required: [true, "Product description is required"],
+  },
+  price: {
+    type: Number,
+    required: [true, "Product price is required"],
+  },
+  stock: {
+    type: Number,
+    required: [true, "Product stock is required"],
+  },
+  categories: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Category",
+    }
+  ],
+  imageUrl: {
+    type: String,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+const Product = mongoose.model("Product", productSchema);
+
+export default Product;
---
+++ models/User.js
@@ -0,0 +1,32 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema({
+  fullname: {
+    type: String,
+    required: [true, "Fullname is required"],
+  },
+  email: {
+    type: String,
+    required: [true, "Email is required"],
+    unique: true,
+    lowercase: true,
+    trim: true,
+  },
+  password: {
+    type: String,
+    required: [true, "Password is required"],
+  },
+  role: {
+    type: String,
+    enum: ["user", "admin"],
+    default: "user",
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+const User = mongoose.model("User", userSchema);
+
+export default User;
