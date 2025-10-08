7a9f83f8daac13b526131894e4a8d2e58bb49ec5
ElFirdaous28 <elmokhtarifirdaous@gmail.com>
2025-10-08 16:46:36 +0100

    get user by id EMA-19

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -61,3 +61,18 @@ export const getUsers = async (req, res) => {
     res.status(500).json({ error: "Server error" });
   }
 }
+
+export const getUserById = async (req, res) => {
+  try {
+    const user = await User.findById(req.params.id);
+
+    if (!user) {
+      return res.status(404).json({ error: "User not found" });
+    }
+
+    res.status(200).json(user);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: "Server error" });
+  }
+}
