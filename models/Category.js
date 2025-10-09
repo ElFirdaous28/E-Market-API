eab9844e8d7d84c108353ae6da2f26a4d3ef895d
ElFirdaous28 <elmokhtarifirdaous@gmqil.com>
2025-10-09 12:10:24 +0100

    EMA-21 soft delets -categories

--- models/Category.js
+++ models/Category.js
@@ -11,7 +11,32 @@ const categorySchema = new mongoose.Schema({
         type: Date,
         default: Date.now,
     },
+    deletedAt: {
+        type: Date,
+        default: null,
+    },
 });
+
+// Helper: soft delete
+categorySchema.methods.softDelete = function () {
+    this.deletedAt = new Date();
+    return this.save();
+};
+
+// Helper: restore
+categorySchema.methods.restore = function () {
+    this.deletedAt = null;
+    return this.save();
+};
+
+// Query helper: only get non-deleted categories
+categorySchema.query.notDeleted = function () {
+    return this.where({ deletedAt: null });
+};
+
+categorySchema.query.deleted = function () {
+    return this.where({ deletedAt: { $ne: null } });
+};
 
 const Category = mongoose.model("Category", categorySchema);
 
--- routes/categoryRouter.js
+++ routes/categoryRouter.js
@@ -6,9 +6,14 @@ import { categorySchema } from "../validations/categorySchema.js";
 const router = express.Router();
 
 router.post("/", validate(categorySchema), CategoryController.createCategory);
+router.get("/", CategoryController.getCategories);
+router.get("/deleted", CategoryController.getDeletedCategories);
+
 router.patch("/:id", validate(categorySchema), CategoryController.updateCategory);
 router.delete("/:id", CategoryController.deleteCategory);
-router.get("/", CategoryController.getCategories);
 router.get("/:id", CategoryController.getCategoryById);
+
+router.delete("/:id/soft", CategoryController.softDeleteCategory);
+router.patch("/:id/restore", CategoryController.restoreCategory);
 
 export default router;
