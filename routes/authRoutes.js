910f08aedd544838f21d6938bd4042f341b7d64f
ElFirdaous28 <elmokhtarifirdaous@gmqil.com>
2025-10-13 10:23:21 +0100

    EMA-25 authentication -fix

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,10 +1,14 @@
 import express from "express";
 import { register, login } from "../controllers/authController.js";
 import { isAuthenticated, isAdmin } from "../middlewares/auth.js";
+import validate from "../middlewares/validate.js";
+import { userSchema } from "../validations/userSchema.js";
 const router = express.Router();
 
-router.post("/register", register);
-router.post("/login", login);
+const loginSchema = userSchema.pick(["email", "password"]);
+
+router.post("/register", validate(userSchema), register);
+router.post("/login",validate(loginSchema), login);
 
 // Example of protected route
 router.get("/profile", isAuthenticated, isAdmin, (req, res) => {
